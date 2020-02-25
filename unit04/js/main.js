8311258d56531c3ce28f05f51dc437d1c056a03f
Kristin <kbrandtsojo@gmail.com>
2020-02-25 10:00:36 -0700

    added fonts and styling

--- unit04/js/main.js
+++ unit04/js/main.js
@@ -7,6 +7,8 @@ function getRandomIntInclusive(min, max) {
   console.log(getRandomIntInclusive(1,12))
 
   let month = getRandomIntInclusive(1,12)
+  
+  let day = getRandomIntInclusive(1,30)
 
   function getMonthName(month) {
 let name 
@@ -53,3 +55,39 @@ default:
 }
 return name
   }
+
+  let fate = getRandomIntInclusive(1,5)
+console.log(fate)
+
+  function getFortune(fate) {
+let message
+switch (fate) {
+case 1: 
+message = "win 100 million dollars in the lotto"
+break;
+case 2: 
+message = 'meet the President of the United States'
+break;
+case 3: 
+message = 'inherit a large fortune from a distant relative'
+break;
+case 4: 
+message = 'lose all your teeth in an accident'
+break;
+case 5: 
+message = 'find the fountain of youth'
+break;
+default: 
+message = 'win a contest for a brand new car'
+break;
+      }
+return message
+  }
+
+
+  const monthName = getMonthName(month)
+  const fortuneGenerator = getFortune(fate)
+  
+  const fortuneRevealed = `On ${monthName} ${day}, you will ${fortuneGenerator}.`
+
+  document.querySelector('#fortune').innerText = fortuneRevealed
