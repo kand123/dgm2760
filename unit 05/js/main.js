8ae8ed46471cf117ddbab47fd81da9a2d1465c3d
Kristin <kbrandtsojo@gmail.com>
2020-03-02 15:36:01 -0700

    fixed js on rest of buttons

--- unit 05/js/main.js
+++ unit 05/js/main.js
@@ -1,6 +1,6 @@
 // create an array with 4 trees 
 
-const trees = ['aspen', 'Oak', 'ash', 'Maple']
+let trees = ['aspen', 'Oak', 'ash', 'Maple']
 const errorElement = document.querySelector('#error')
 const displayResults = document.querySelector('#displayResults')
 
@@ -32,10 +32,57 @@ document.querySelector('#add_pear').onclick = () => {
 
 document.querySelector('#remove_tree1').onclick = () => {
     if (trees.length > 0) {
-trees.shift()
+trees.shift() 
 listTrees()
     } else {
         errorElement.textContent = 'Uh oh. I cannot remove the first tree. There are no trees.'
     }
 }
 
+document.querySelector('#remove_tree2').onclick = () => {
+    if (trees.length > 1) {
+trees.splice(1,1)
+listTrees()
+    } else {
+        errorElement.textContent = 'Uh oh. I cannot remove the second tree. There are no trees.'
+    }
+}
+
+document.querySelector('#remove_treeLast').onclick = () => {
+    if (trees.length > 0) {
+trees.pop()
+listTrees()
+    } else {
+        errorElement.textContent = 'Uh oh. I cannot remove the last tree. There are no trees.'
+    }
+}
+
+
+document.querySelector('#sortTrees').onclick = () => {
+ trees.sort()
+ listTrees()
+}
+
+document.querySelector('#lowerTrees').onclick = () => {
+    trees = trees.join(" ").toLowerCase()
+    trees = trees.split(" ",trees.length)
+    
+    console.log(trees.length)
+    listTrees()
+}
+
+document.querySelector('#showName3').onclick = () => {
+    if (trees.length > 2) {
+errorElement.textContent = `${trees[2]}`
+    } else {
+        errorElement.textContent = 'Uh oh. There are not enough trees.'
+    }
+}
+
+document.querySelector('#showName4').onclick = () => {
+    if (trees.length > 3) {
+errorElement.textContent = `${trees[3]}`
+    } else {
+        errorElement.textContent = 'Uh oh. There are not enough trees.'
+    }
+}
