0b88867c2337def98edb920378682bcb9da87591
Kristin <kbrandtsojo@gmail.com>
2020-03-23 12:45:30 -0600

    initial commit

---
+++ unit06/js/main.js
@@ -0,0 +1,9 @@
+function tellStory(){
+//nounArray = querySelector('#noun') //lowercase //string split()
+//repeat for adjective and verbs
+
+const myStory =`Once upon a time there were four ${nounArray[0]}s named `
+}
+
+//get a reference to the querySelector('#story') = myStory
+
