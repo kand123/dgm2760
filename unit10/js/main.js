9d5a8e9f56eb4ff1e824fd97449c37eccdc1da47
Kristin <kbrandtsojo@gmail.com>
2020-04-17 00:31:27 -0600

    testing123

--- unit10/js/main.js
+++ unit10/js/main.js
@@ -2,7 +2,7 @@
 
 async function getHotelData() {
     try {
-const response = await fetch('../hotel.json')
+const response = await fetch("../hotel.json")
 return await response.json() //return the JSON object
 
     } catch (error) {
